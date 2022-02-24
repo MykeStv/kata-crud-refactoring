fc32b045875d230c59cb4440dfbb8a8358069036
MykeStv <mykestv22@gmail.com>
2022-02-24 14:45:22 -0500

    Se ingresó todo al list en back controller

--- back/src/main/java/co/com/sofka/crud/controller/ToDoListController.java
+++ back/src/main/java/co/com/sofka/crud/controller/ToDoListController.java
@@ -1,12 +1,11 @@
 package co.com.sofka.crud.controller;
 
+import co.com.sofka.crud.model.ToDo;
 import co.com.sofka.crud.model.ToDoList;
+import co.com.sofka.crud.model.ToDoModel;
 import co.com.sofka.crud.service.ToDoListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/list")
@@ -23,5 +22,10 @@ public class ToDoListController {
     @PostMapping
     public ToDoList saveTodoList(ToDoList todoList) {
         return this.listService.saveList(todoList);
+    }
+
+    @PutMapping(path = "/{listid}")
+    public ToDoList addTodo(@PathVariable("listid") Long listid,@RequestBody ToDo todo) {
+        return this.listService.addTodo(listid, todo);
     }
 }
--- back/src/main/java/co/com/sofka/crud/service/ToDoListService.java
+++ back/src/main/java/co/com/sofka/crud/service/ToDoListService.java
@@ -1,7 +1,10 @@
 package co.com.sofka.crud.service;
 
+import co.com.sofka.crud.model.ToDo;
 import co.com.sofka.crud.model.ToDoList;
+import co.com.sofka.crud.model.ToDoModel;
 import co.com.sofka.crud.repository.ToDoListRepository;
+import co.com.sofka.crud.repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +13,8 @@ public class ToDoListService {
 
     @Autowired
     private ToDoListRepository toDoListRepository;
+    @Autowired
+    private ToDoRepository todoRepository;
 
     //Metodo para obtener los datos
     public Iterable<ToDoList> getList() {
@@ -19,5 +24,37 @@ public class ToDoListService {
     //Metodo para guardar datos
     public ToDoList saveList(ToDoList todoList) {
         return this.toDoListRepository.save(todoList);
+    }
+
+
+    /*public ToDoList saveTodo(Long listid, ToDoModel todoM ) {
+        var listTodo = this.toDoListRepository.findById(listid).orElseThrow();
+
+        var todo = new ToDo();
+
+        todo.setId(todoM.getId());
+        todo.setName(todoM.getName());
+        todo.setCompleted(todoM.isCompleted());
+
+        //adicionar un nuevo to-do a la lista
+        listTodo.getTodo().add(todo);
+
+        return this.toDoListRepository.save(listTodo);
+
+    }*/
+    //Guardar todo en list
+    public ToDoList addTodo(Long listid, ToDo todo ) {
+        var listTodo = this.toDoListRepository.findById(listid).orElseThrow();
+
+        //adicionar un nuevo to-do a la lista
+        listTodo.getTodo().add(todo);
+
+        return this.toDoListRepository.save(listTodo);
+
+    }
+
+    //Eliminar un todo de la lista
+    public void deleteTodoById(Long id) {
+        this.todoRepository.deleteById(id);
     }
 }
