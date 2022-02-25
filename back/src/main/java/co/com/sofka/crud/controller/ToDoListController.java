44eec194374953139b13f4ef1637d36fb0c8b894
MykeStv <mykestv22@gmail.com>
2022-02-25 14:05:54 -0500

    Se realizó el fetch con el api, y se probaron los metodos get y post, se implementará Context en react

--- back/src/main/java/co/com/sofka/crud/controller/ToDoListController.java
+++ back/src/main/java/co/com/sofka/crud/controller/ToDoListController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/list")
+@CrossOrigin(origins = "*")
 public class ToDoListController {
 
     @Autowired
--- crud-front/src/App.js
+++ crud-front/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddListForm from './components/AddListForm';
 import ListGomponet from './components/ListGomponet';
 
@@ -6,22 +6,52 @@ import ListGomponet from './components/ListGomponet';
 
 function App() {
 
-  const DATA = [
+  /* const DATA = [
     { id: 1, name: 'Comer', todo:[{ id: 1, name: 'dinner', completed: true }] },
     { id: 2, name: 'Dormir', todo:[] },
     { id: 3, name: 'Estudiar', todo:[] },
-  ]
+  ] */
 
   //Estado de las listas
-  const [lists, setLists] = useState(DATA)
+  const [lists, setLists] = useState([])
+
+  const url = 'http://localhost:8080/list';
+  useEffect(() => {
+    
+    const getData = async () => {
+      //Trae la data
+      const DATA = await fetch(url)
+        .then(res => res.json());
+      console.log( DATA);
+      setLists(DATA)
+    }
+    
+    getData()
+
+  }, [])
+  
 
   const addList = (list) => {
-    list.id = lists[lists.length - 1].id + 1;
+    // list.id = lists[lists.length - 1].id + 1;
 
-    setLists([
-      ...lists,
-      list
-    ]);
+    const req = {
+      id: null,
+      name: list.name,
+      todo: []
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(req),
+      
+    }).then(
+      res => res.json()
+    )
+
+    
 
   }
 
